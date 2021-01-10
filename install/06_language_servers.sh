d4dac507db6efd3e7e4a1bdb76fa695bba687e42
Jason Adam <22adamj22@gmail.com>
2021-01-10 14:13:58 -0700

    update coc settings

--- install/06_language_servers.sh
+++ install/06_language_servers.sh
@@ -4,6 +4,7 @@ set -e;
 
 # Install Language Servers for coc.nvim
 lang_servers() {
+    curl -sL install-node.now.sh/lts | bash;
 	npm install -g dockerfile-language-server-nodejs;
 	npm i -g bash-language-server;
 }
