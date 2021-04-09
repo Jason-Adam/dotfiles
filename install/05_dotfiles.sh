fb3acce6d8edc2079191dfc8a13036c01a6d4ffc
Jason Adam <22adamj22@gmail.com>
2021-04-09 13:15:53 -0700

    updated symlinks and added gitconfig vars

--- install/05_dotfiles.sh
+++ install/05_dotfiles.sh
@@ -8,6 +8,7 @@ ln -s -f "$HOME"/code/dotfiles/.zshrc "$HOME"/.zshrc
 ln -s -f "$HOME"/code/dotfiles/.tmux.conf "$HOME"/.tmux.conf
 ln -s -f "$HOME"/code/dotfiles/coc-settings.json "$HOME"/.vim/coc-settings.json
 ln -s -f "$HOME"/code/dotfiles/.gitconfig "$HOME"/.gitconfig
+ln -s -f "$HOME"/code/dotfiles/.gitignore_global "$HOME"/.gitignore_global
 ln -s -f "$HOME"/code/dotfiles/Makevars "$HOME"/.R/Makevars
 ln -s -f "$HOME"/code/dotfiles/.vimrc "$HOME"/.ideavimrc
 ln -s -f "$HOME"/code/dotfiles/kitty.conf "$HOME"/.config/kitty/kitty.conf
